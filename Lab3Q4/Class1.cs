1a443856b0697c933420e2e3be3679fd812242bf
Astha5 <astha05101997@gmail.com>
2019-09-10 15:55:36 +0530

    Add files via upload

---
+++ Lab3Q4/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3Q4
+{
+    public class SupplierTest
+    {
+        private int _supplierId;
+        private string _supplierName;
+        private string _city;
+        private string _email;
+        private string _phone;
+
+        public int SupplierId { get => _supplierId; set => _supplierId = value; }
+        public string SupplierName { get => _supplierName; set => _supplierName = value; }
+        public string City { get => _city; set => _city = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string Phone { get => _phone; set => _phone = value; }
+     
+
+        public void Acceptdetails()
+        {
+          
+            Console.WriteLine("Enter the Supplier Id");
+           SupplierId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter SupplierName");
+            SupplierName = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            City = Console.ReadLine();
+            Console.WriteLine("Enter Email");
+            Email = Console.ReadLine();
+            Console.WriteLine("Enter Phone");
+            Phone = Console.ReadLine();
+
+        }
+        public void Displaydetails()
+        {
+            Console.WriteLine(" Supplier Id: " + SupplierId);
+            Console.WriteLine(" SupplierName: " + SupplierName);
+            Console.WriteLine(" City: " + City);
+            Console.WriteLine(" Email: " + Email);
+            Console.WriteLine(" Phone: " + Phone);
+        }
+    }
+}
---
+++ Lab3Q4/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab3Q4;
+namespace Lab3Q4
+{
+   public class Class2
+    {
+        static void Main()
+        {
+            SupplierTest supplier= new SupplierTest();
+            supplier.Acceptdetails();
+            supplier.Displaydetails();
+
+        }
+        }
+    }
+
