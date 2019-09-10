8f202e31fe3ae8a57207d85afc3cda77509d33dc
Astha5 <astha05101997@gmail.com>
2019-09-10 11:06:39 +0530

    Add files via upload

---
+++ Lab3Q2/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3Q2
+{
+    class Class1
+    {
+        static void Main()
+        {
+            Bird b = new Bird("Eagle", double.Parse("200"));
+            b.fly();
+            b.fly(int.Parse("300"));//9
+        }
+    }
+}
---
+++ Lab3Q2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lab3Q2
+{
+   public class Bird//1
+    {
+        public double Maxheight;
+       
+        public string Name;
+        public Bird()
+        {
+            this.Name = "Mountain Eagle";//2
+            this.Maxheight = 500;//3
+        }
+        public Bird(string birdname,double max_ht)
+        {
+            this.Name = "Another Bird";//4
+            this.Maxheight = 0;//5
+
+        }
+        public void fly()
+        {
+            Console.WriteLine(this.Name + "is flying at altitude" + this.Maxheight);
+        }
+        public void fly(int AtHeight)//6
+        {
+            if(AtHeight<=this.Maxheight)
+            {
+                Console.WriteLine(this.Name + "flying at" + AtHeight.ToString());
+            }
+            else //7
+            {
+                Console.WriteLine(this.Name + "cannot fly at this height");//8
+            }
+
+        }
+
+        
+    }
+}
